f03d053f616c7979e816ebe66fba01c658489bd2
Tom Corley <thomcorley@gmail.com>
2018-07-17 11:34:30 +0100

    Update index controller with test structure

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -9,12 +9,33 @@ class IndexController < ApplicationController
     token_endpoint: "api.sandbox.freeagent.com/v2/token_endpoint"
   }
 
+
   def authorize
-  	
+  	@url = CONFIG[:auth_endpoint] + authorization_params
   end
 
   def callback
+    auth_code = params[:code]
 
+    token_response = HTTParty.post("https://#{CONFIG[:client_id]}:#{CONFIG[:client_secret]}@#{CONFIG[:token_endpoint]}",
+      headers: {
+        "Content-Type" => "application/x-www-form-urlencoded;charset=UTF-8"
+      },
+      body: {
+        "grant_type" => "authorization_code",
+        "code" => auth_code,
+        "redirect_uri" => CONFIG[:redirect_uri_2]
+      })
+
+      access_token = token_response["access_token"]
+
+      bank_accounts = HTTParty.get("https://api.sandbox.freeagent.com/v2/bank_accounts/id",
+        headers: {
+          "Authorization" => "Bearer #{access_token}",
+          "User-Agent" => "FreeAgent Api Demo"
+        })
+
+      puts bank_accounts
   end
 
   private
@@ -23,7 +44,7 @@ class IndexController < ApplicationController
     params = {
       client_id: CONFIG[:client_id],
       response_type: "code",
-      redirect_uri: CONFIG[:redirect_uri]
+      redirect_uri: CONFIG[:redirect_uri_2]
     }
 
     URI.encode_www_form(params)
